ac7ec18c679dc825d9ba1263d9f006e19ba4b0a0
BrunoMatheus10 <brunomatheus88300432@gmail.com>
2024-09-18 11:09:03 -0300

    Add files via upload

---
+++ atividade-tema10-8.py
@@ -0,0 +1,10 @@
+# Criar uma tupla com os signos do zodíaco
+signos_zodiaco = (
+    "Áries", "Touro", "Gêmeos", "Câncer", "Leão", "Virgem",
+    "Libra", "Escorpião", "Sagitário", "Capricórnio", "Aquário", "Peixes"
+)
+
+# Exibir os valores da tupla em tela
+print("Signos do Zodíaco:")
+for signo in signos_zodiaco:
+    print(signo)
